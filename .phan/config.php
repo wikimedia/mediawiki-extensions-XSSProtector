43b403bf7fd1d964a5e0680aeb8d7fbb8c31d1a3
Brian Wolff <bawolff+wn@gmail.com>
2025-09-06 14:18:08 -0700

    Fix terrible hack
    Also fix tests
    Change-Id: I6460c9614cf1d52f8651f177ebece758287ae548
    
    Change-Id: Idd33f88b1849502030dba24b930e39b26d26258e

--- .phan/config.php
+++ .phan/config.php
@@ -1,4 +1,9 @@
 <?php
 $config = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
 
+$config['suppress_issue_types'] = array_merge( $config['suppress_issue_types'], [
+	'PhanRedefinedClassReference',
+	'PhanRedefineClass',
+] );
+
 return $config;
