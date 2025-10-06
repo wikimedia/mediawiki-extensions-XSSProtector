b5d0cd022ea380cdfa0032ac55727e5940c31f94
Brian Wolff <bawolff+wn@gmail.com>
2025-10-06 07:18:23 -0700

    Rm phan supression no longer needed
    Change-Id: I38a88f16a31c373106a1b0e73ca759b9a9028e51

--- .phan/config.php
+++ .phan/config.php
@@ -1,9 +1,3 @@
 <?php
 $config = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
-
-$config['suppress_issue_types'] = array_merge( $config['suppress_issue_types'], [
-	'PhanRedefinedClassReference',
-	'PhanRedefineClass',
-] );
-
 return $config;
