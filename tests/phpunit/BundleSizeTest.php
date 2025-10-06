a3dfb6594555295514d17383b20083219312830c
Brian Wolff <bawolff+wn@gmail.com>
2025-10-06 06:48:32 -0700

    Fix bundlesize tests
    Change-Id: I962d830bef7c5edeeceecb90cc3ec9ec7c80095d

---
+++ tests/phpunit/BundleSizeTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace MediaWiki\Extensions\XSSProtector\Tests;
+
+class BundleSizeTest extends \MediaWiki\Tests\Structure\BundleSizeTestBase {
+
+	/** @inheritDoc */
+	public function getBundleSizeConfig(): string {
+		return dirname( __DIR__, 2 ) . '/bundlesize.config.json';
+	}
+}
